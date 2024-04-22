772938b9231a902c336ee29e1c7cb2b6ef28d142
edwin99 <j.chen1150964115@gmail.com>
2024-04-22 18:57:24 +0800

    Update settings.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '__=s9@oixaun$x^g7-4#10wf_*7zvb8)kl1$j82fj&cyq%^o^3'
 # 部署到线上时为 False; 读者在本地调试时请修改为 True
 DEBUG = True
 
-ALLOWED_HOSTS = ['47.101.11.42','liedwin.top']
+ALLOWED_HOSTS = ['liedwin.top','47.101.11.42']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
